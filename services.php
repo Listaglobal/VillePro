890f899fd1bea75aa66b7708549310fa4425ac86
afolabi49 <108076216+McPherson49@users.noreply.github.com>
2024-05-10 14:08:53 +0100

    Services Component

--- services.php
+++ services.php
@@ -35,6 +35,106 @@
     </section>
     <!-- breadcrumb area end -->
 
+    <!-- about area start -->
+    <section class="about-area about-area-2 about-area-3 pt-130 pb-145">
+        <div class="section-heading mb-60">
+            <h2 class="section-title text-center">Our Services</h2>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="about-thumb-wrap about-thumb-wrap-3 mt-60">
+                        <div class="about-thumb-big">
+                            <div class="about-thumb-small">
+                                <img src="" alt="">
+                            </div>
+                            <img src="assets/images/about/03.jpg" alt="">
+                            <span class="shape">
+                                <img src="assets/images/shape.png" alt="">
+                            </span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="about-content about-content-3">
+                        <div class="section-heading mb-35">
+                            <h2 class="section-title">Domicilliary Care
+                                With Our Professional
+                                &amp; Adict Doctors</h2>
+                            <p>Our domiciliary care services are built around you. Our core focus is to support you to maintain your 
+                                desired lifestyle, and this can range from a visit a day to 24-hour live-in care. We believe that every 
+                                client is unique, having their own individual preferences as well as needs. Hence our duty is to keep you in 
+                                control and provide you with the much-needed domiciliary care as well as support that you want, 
+                                right when and where you want it.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row mt-60">
+                <div class="col-lg-6">
+                    <div class="about-content about-content-3">
+                        <div class="section-heading mb-35">
+                            <h2 class="section-title">Domicilliary Care
+                                With Our Professional
+                                &amp; Adict Doctors</h2>
+                            <p>Our domiciliary care services are built around you. Our core focus is to support you to maintain your 
+                                desired lifestyle, and this can range from a visit a day to 24-hour live-in care. We believe that every 
+                                client is unique, having their own individual preferences as well as needs. Hence our duty is to keep you in 
+                                control and provide you with the much-needed domiciliary care as well as support that you want, 
+                                right when and where you want it.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="about-thumb-wrap about-thumb-wrap-3 mt-60">
+                        <div class="about-thumb-big">
+                            <div class="about-thumb-small">
+                                <img src="" alt="">
+                            </div>
+                            <img src="assets/images/about/03.jpg" alt="">
+                            <span class="shape">
+                                <img src="assets/images/shape.png" alt="">
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row mt-60">
+                <div class="col-lg-6">
+                    <div class="about-thumb-wrap about-thumb-wrap-3 mt-60">
+                        <div class="about-thumb-big">
+                            <div class="about-thumb-small">
+                                <img src="" alt="">
+                            </div>
+                            <img src="assets/images/about/03.jpg" alt="">
+                            <span class="shape">
+                                <img src="assets/images/shape.png" alt="">
+                            </span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="about-content about-content-3">
+                        <div class="section-heading mb-35">
+                            <h2 class="section-title">Domicilliary Care
+                                With Our Professional
+                                &amp; Adict Doctors</h2>
+                            <p>Our domiciliary care services are built around you. Our core focus is to support you to maintain your 
+                                desired lifestyle, and this can range from a visit a day to 24-hour live-in care. We believe that every 
+                                client is unique, having their own individual preferences as well as needs. Hence our duty is to keep you in 
+                                control and provide you with the much-needed domiciliary care as well as support that you want, 
+                                right when and where you want it.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- about area end -->
+
     <!-- footer start -->
     <?php include 'includes/footer.php'; ?>
     <!-- footer end -->
